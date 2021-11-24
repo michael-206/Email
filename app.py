131b4c8525458f819e1e3d5dca23888030bed3fe
michael-206 <michaelmoo206@gmail.com>
2021-11-24 16:28:36 -0500

    main

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Email, Documents, Questions
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Email': Email, 'Documents': Documents, 'Questions': Questions}
